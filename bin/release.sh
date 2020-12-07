0db2c44f40b3295fa6f67ec24367c3185170860a
Robert Egglestone <robertegglestone@gmail.com>
2020-12-08 01:05:15 +1300

    Force push on release to remove commit

--- bin/release.sh
+++ bin/release.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 branch=(git branch --show-current)
+branch=(git branch --show-current)
 git branch -D latest || true
 git branch -c main latest
 git switch latest
 npm run build
 git add -f dist/index.js
 git commit -m Release
-git push
+git push -f origin latest
 git switch $branch
