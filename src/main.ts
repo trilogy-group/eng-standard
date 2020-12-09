744aa20e6470711fc8d3c8f0b087be19467ac1e0
Robert Egglestone <robertegglestone@gmail.com>
2020-12-10 01:19:55 +1300

    debug environment

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,12 @@ import { container } from 'tsyringe';
 import  './OctokitFactory';
 import { ComplianceChecker } from './ComplianceChecker';
 
+// this is for debugging GitHub actions issues
+console.log('Environment:');
+for(const key in process.env) {
+    console.log(`${key}: ${process.env[key]}`);
+}
+console.log('')
+
 const complianceChecker = container.resolve(ComplianceChecker);
 complianceChecker.main();
