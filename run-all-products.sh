b5b0f78b1ef952c5328dc1909c880ac0db062a90
Robert Egglestone <robertegglestone@gmail.com>
2021-05-29 00:11:15 +1200

    Remove deprecated

--- run-all-products.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cat products.tsv | while IFS=$'\t' read INPUT_PRODUCT_NAME INPUT_REPOSITORY; do
-	export INPUT_PRODUCT_NAME INPUT_REPOSITORY
-	echo $INPUT_PRODUCT_NAME
-	ts-node src/main.ts
-done
