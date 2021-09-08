91bab67f59e2f9e1fdb6241eb5ce0ffded7a5563
Robert Egglestone <robert.egglestone@trilogy.com>
2021-09-08 15:45:28 +1200

    Update gitpod-check.sh

--- bin/gitpod-check.sh
+++ bin/gitpod-check.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+set -e
 if [ -z "${GITHUB_TOKEN}" ]; then
   echo -e "\e[31mPlease set the environment variable GITHUB_TOKEN at https://trilogy.devspaces.com/variables\e[0m\nLearn more at https://github.com/trilogy-group/eng-base-ts/blob/main/doc/github-packages.md"
-  return 1
+  exit 1
 fi
